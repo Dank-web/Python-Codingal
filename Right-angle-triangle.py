ad39fdf1c5fae76385016579f6a9ce39314edde4
Dank-web <nusratjahanmoon27@gmail.com>
2026-02-22 00:11:13 +0600

    Add files via upload

---
+++ Right-angle-triangle.py
@@ -0,0 +1,11 @@
+n = 9
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    print("".join(str(j) for j in range(1, i + 1)) +
+          "".join(str(j) for j in range(i - 1, 0, -1)))
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i),end="")
+    print("".join(str(j) for j in range(1, i + 1)) +
+          "".join(str(j) for j in range(i - 1, 0, -1)))
