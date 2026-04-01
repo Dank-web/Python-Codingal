4bfea624b3c5e90c53ee10889686afe50f62a24c
Dank-web <nusratjahanmoon27@gmail.com>
2026-04-01 19:55:59 +0600

    Add files via upload

---
+++ playing-list.py
@@ -0,0 +1,22 @@
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Original List :", L)
+
+count = 0
+# Finding the sum 
+for i in L:
+    count += i
+#divide the total elements by
+#number of elements
+avg = count/len(L)
+
+print("sum = ", count)
+print("average = ", avg)
+
+#sorting the elements of the list
+L.sort()
+print(L)
+#printing the first element
+print("Smallest element is:",L[0])
+
+#Printing the last element
+print("Largest element is:", L[-1])
