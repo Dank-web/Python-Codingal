59ce207dc9ade8485abe48be739d06f99dc45b64
Dank-web <nusratjahanmoon27@gmail.com>
2026-04-05 17:46:53 +0600

    Add files via upload

---
+++ Checking-the-frequency.py
@@ -0,0 +1,18 @@
+#Initialize dictionary
+test_dict = {"Codingal": 2, "is" : 1, "best" : 2, "for" : 2, "Codingal" : 1}
+
+# printing original dictionary
+print("The original dictionary : " + str(test_dict))
+
+#Initislise
+k = 1
+
+#using loop
+#select key values in dictionary
+res = 0
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+
+#printing result
+print("Frequency of K is : " + str(res))
---
+++ rid_of_duplicates.py
@@ -0,0 +1,17 @@
+#Dictionary of students (id -> details)
+student_data = {
+    "id1": {"name": "Sara", "class": "V", "subject_integration": "English, Math, Science"},
+    "id2": {"name": "David", "class": "V", "subject_integration": "English, Math, Science"},
+    "id3": {"name": "Sara", "class": "V", "subject_integration": "English, Math, Science"},
+    "id4": {"name": "Surya", "class": "V", "subject_integration": "English, Math, Science"},
+}
+result = {}
+seen_keys = [] #using a list instead of set
+for student_id, details in student_data.items():
+    unique_key = (details["name"], details["class"], details["subject_integration"])
+    if unique_key not in seen_keys:
+        seen_keys.append(unique_key)
+        result[student_id] = details
+#print output line by line
+for k, v in result.items():
+    print(k, ":", v)
