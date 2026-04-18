4efb09c8483b709f962102ede41220c3f39b6e2a
Dank-web <nusratjahanmoon27@gmail.com>
2026-04-19 00:41:27 +0600

    Add files via upload

---
+++ set-intersection.py
@@ -0,0 +1,10 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nInternational of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
+setz = setx.union(sety)
+print(setz)
