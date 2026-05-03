d6928c4e4929e664a54f13ad27ee5dcff9a91de0
Dank-web <nusratjahanmoon27@gmail.com>
2026-05-03 23:24:08 +0600

    Add files via upload

---
+++ abstract-method.py
@@ -0,0 +1,25 @@
+#Import necesssary Modules
+from abc import ABC, abstractclassmethod
+#Create base class
+
+class Absclass(ABC):
+
+    #Function to print a value
+    def print(self,x):
+        print("Passed value: ", x)
+
+    #Abstract Method
+    @abstractclassmethod
+    def task(self):
+        print("we are inside Absclass task")
+
+#Create sub class
+class test_class(Absclass):
+    
+    def task(self):
+        print("We are inside test_class task")
+
+#object of test_class created
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)
