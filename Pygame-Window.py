2ad7907888eeff16e6514809feb449a1c5012fe6
Grim <nusratjahanmoon27@gmail.com>
2026-06-07 00:02:33 +0600

    Add files via upload

---
+++ Pygame-Window.py
@@ -0,0 +1,12 @@
+#import necessary libraries
+from tkinter import *
+
+#Create window
+window = Tk()
+
+#Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('800x600')
+
+#Start the GUI event loop
+window.mainloop()
